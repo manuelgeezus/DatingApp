19a52fd14d00a0ebdf556f24aca8fc4b9cb20f2f
Manuel Castro <manuel.620@gmail.com>
2022-08-09 22:00:59 -0500

    End Of Section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountsService } from './_services/accounts.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(
-    private accountService: AccountsService
+    private accountService: AccountsService, private presence: PresenceService
   ) {}
 
   ngOnInit() {
@@ -22,6 +23,10 @@ export class AppComponent implements OnInit {
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
-    this.accountService.setCurrentUser(user);
+    if(user){
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
+  
   }
 }
